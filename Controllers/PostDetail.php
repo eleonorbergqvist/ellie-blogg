52a33f411d898ce81ee96518be91ba56a3b0d00b
Eleonore Bergqvist <eleonore296@gmail.com>
2017-11-09 15:08:42 +0100

    Add example checkboxes for category

--- Controllers/PostDetail.php
+++ Controllers/PostDetail.php
@@ -6,13 +6,19 @@ use \Views\PostDetail as PostDetailView;
 
 
 class PostDetail {
-    
+
     function get($params){
         $postId = $params[0];
         $post = Post::get($postId);
 
         //get select blablab mysql
         // var_dump(Db::getInstance());
+        /*
+        return new Response('Views/postdetail.php', [
+            'post' => $post,
+        ]);
+        */
+
         return new PostDetailView([
             'post' => $post,
         ]);

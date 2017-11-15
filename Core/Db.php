577dfb1b9abf121d8e01e0d50e7a5e482bbebf59
Eleonore Bergqvist <eleonore296@gmail.com>
2017-11-15 20:02:34 +0100

    Fix issue with encoding in db layer

--- Core/Db.php
+++ Core/Db.php
@@ -15,7 +15,14 @@ class Db {
         $db = "ellie_blogg";
 
         try {
-            $pdo = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+            $pdo = new PDO(
+                "mysql:host=$servername;dbname=$db", 
+                $username, 
+                $password, 
+                array(
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+                )
+            );
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch(PDOException $e) {
